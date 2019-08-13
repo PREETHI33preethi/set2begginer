7dd14f86dd37e70920979cc274249fdb184dd65f
PREETHI33preethi <48052863+PREETHI33preethi@users.noreply.github.com>
2019-08-13 08:02:56 +0530

    Update Fact.c

--- Fact.c
+++ Fact.c
@@ -4,9 +4,7 @@ int main()
     int n, i;
     unsigned long long factorial = 1;
     scanf("%d",&n);
-    if (n < 0)
-        printf("Error! Factorial of a negative number doesn't exist.");
-    else
+    if(n>0)
     {
         for(i=1; i<=n; ++i)
         {
