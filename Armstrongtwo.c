1e7b68cb8680f5fbd3bc1ae0d0a1f2edd340c279
PREETHI33preethi <48052863+PREETHI33preethi@users.noreply.github.com>
2019-08-13 08:30:18 +0530

    Update Armstrongtwo.c

--- Armstrongtwo.c
+++ Armstrongtwo.c
@@ -4,7 +4,7 @@ int main()
 {
     int low, high, i, temp1, temp2, remainder, n = 0, result = 0;
     scanf("%d %d", &low, &high);
-    printf("Armstrong numbers between %d an %d are: ", low, high);
+    printf("%d%d", low, high);
     for(i = low + 1; i < high; ++i)
     {
         temp2 = i;
@@ -20,7 +20,8 @@ int main()
             result += pow(remainder, n);
             temp2 /= 10;
         }
-        if (result == i) {
+        if (result == i)
+       {
             printf("%d ", i);
         }
         n = 0;
